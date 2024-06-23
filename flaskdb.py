fc3fb326e04d99c8ddfc5be7c32ecc388564b130
MahshadRoustapour <mahshadroustapour1@gmail.com>
2024-06-23 17:22:48 +0430

    page of articles added

--- flaskdb.py
+++ flaskdb.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/images')
 def images():
     return 'images'
+
 @app.route('/home')
 def home():
     return 'home'
+
+@app.route('/articles')
+def articles():
+    return 'articles'
